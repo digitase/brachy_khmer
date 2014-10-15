2ada40bb28d5f5e6628e1fb993e1a56f60274b77
Ben Bai <u5205339@anu.edu.au>
2014-10-16 03:55:18 +1100

    Modified plot_hist.py to allow optional x/y axis limits.

--- scripts/plot_hist.py
+++ scripts/plot_hist.py
@@ -1,16 +1,35 @@
 
+import os
 import sys
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
 filename = sys.argv[1]
-
+sample_name = os.path.splitext(os.path.basename(filename))[0]
 hist = np.loadtxt(filename, dtype=float, delimiter=" ")
 
 plt.plot(hist[:, 0], hist[:, 1])
+plt.suptitle(sample_name)
+plt.xlabel("Kmer abundance")
+plt.ylabel("Frequency")
+
+# If we are looking at the graph interactively, we won't have axis limits
+save_fig = True
+
+try:
+    plt.xlim(0, int(sys.argv[2]))
+    plt.ylim(0, int(sys.argv[3]))
+except IndexError:
+    save_fig = False
+
+if save_fig:
+    plt.savefig(sample_name + ".png", bbox_inches='tight')
+
 plt.draw()
 plt.show()
+
+
 
 
 
