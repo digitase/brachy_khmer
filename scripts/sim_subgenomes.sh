39632d1865f8063b1fc53e53accd25802f7b7146
Ben Bai <u5205339@anu.edu.au>
2014-10-16 05:51:55 +1100

    Added read error rate option to sim_subgenomes.sh

--- scripts/sim_subgenomes.sh
+++ scripts/sim_subgenomes.sh
@@ -3,12 +3,13 @@
 REF_FILE=$1
 TOTAL_READ_NUM=$2
 READ_LEN=$3
-SUBGENOME_NUM=$4
+SEQ_ERROR=$4
+SUBGENOME_NUM=$5
 
 ref_basename=$(basename "$REF_FILE" ".fa.gz")
 args=("$@")
-read_proportions=(${args[@]:4:$SUBGENOME_NUM})
-mutation_rates=(${args[@]:4+$SUBGENOME_NUM:$SUBGENOME_NUM})
+read_proportions=(${args[@]:5:$SUBGENOME_NUM})
+mutation_rates=(${args[@]:5+$SUBGENOME_NUM:$SUBGENOME_NUM})
 
 # Array concatenation with delimiter
 # Source: Nicholas Sushkin, http://stackoverflow.com/questions/1527049/bash-join-elements-of-an-array
@@ -17,7 +18,7 @@ function join { local IFS="$1"; shift; echo "$*"; }
 # Add subgenome proportions and rates to filename
 all_props=$(join , "${read_proportions[@]}")
 all_rates=$(join , "${mutation_rates[@]}")
-out_read_file="$ref_basename.-${all_props}-${all_rates}-.il.fq.gz"
+out_read_file="$ref_basename.-$SEQ_ERROR-${all_props}-${all_rates}-.il.fq.gz"
 : > $out_read_file
 
 echo "Output file: $out_read_file"
@@ -36,7 +37,7 @@ do
 
     # Discard details about location of mutations to /dev/null
     # Usage:   wgsim [options] <in.ref.fa> <out.read1.fq> <out.read2.fq>
-    wgsim -N $read_num -1 $READ_LEN -2 $READ_LEN -r "${mutation_rates[$n]}" "$REF_FILE" $tmp1 $tmp2 >/dev/null
+    wgsim -N $read_num -1 $READ_LEN -2 $READ_LEN -r "${mutation_rates[$n]}" -e $SEQ_ERROR "$REF_FILE" $tmp1 $tmp2 >/dev/null
     pairs join $tmp1 $tmp2 | gzip >> "$out_read_file"
 done
 
